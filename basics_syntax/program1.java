c3a9f180b8e5de76d2996e5b0ecf4a80952dd8c7
earun85 <mailme.earun@gmail.com>
2026-06-06 14:21:01 +0530

    basics syntax

--- basics_syntax/program1.java
+++ basics_syntax/program1.java
@@ -1,5 +1,6 @@
-public class program1{
-    public static void main(String[]args){
+public class program1 {
+    public static void main(String[] args) {
         System.out.println("hello world");
     }
 }
+
