285ab28be0ff904a4afe232d7b405d18bf7f78a2
earun85 <mailme.earun@gmail.com>
2026-06-06 15:29:23 +0530

    notes pushed

--- basics_syntax/program1.java
+++ basics_syntax/program1.java
@@ -1,6 +1,5 @@
-public class program1 {
+public class Program1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Welcome " + args[0]);
     }
 }
-
